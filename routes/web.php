3d37c39620427280a357543660170d78a13f3e2f
haryanto.duwi <haryanto.duwi@gmail.com>
2021-02-25 09:11:35 +0700

    redirect default url to fill kuisioner

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return redirect()->route('kuisioner.index');
 });
 Route::resource('/kuisioner', 'DataUserController');
 Route::get('/login', 'AuthController@showFormLogin')->name('login');
