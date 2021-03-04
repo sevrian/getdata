b4648982e796472316084fe396f616a5ba9a0940
bobbysevri <bobbysevri@gmail.com>
2021-03-04 15:10:57 +0700

    rev migration

--- database/migrations/2021_02_24_033920_create_email_models_table.php
+++ database/migrations/2021_02_24_033920_create_email_models_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
 
 class CreateEmailModelsTable extends Migration
 {
@@ -16,9 +17,9 @@ class CreateEmailModelsTable extends Migration
         Schema::create('email', function (Blueprint $table) {
             $table->id();
             $table->string('email');
-            $table->string('no_tlp', 20);
-            $table->string('nama');
-            $table->boolean('status')->default(1);
+            $table->string('no_tlp', 20)->nullable();
+            $table->string('nama')->nullable();
+            $table->boolean('status')->nullable()->default(true);
             $table->timestamps();
         });
     }
